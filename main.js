f0ace77fafb965554c50e23dcc3975b4292e25b0
Sabimael Añaguaya <sabi.zack93@gmail.com>
2022-08-04 22:20:29 -0400

    add a script to list cart

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const navbarShoppingCart = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
 
+const cardsContainer = document.querySelector('.cards-container')
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamburger.addEventListener('click', toggleMobileMenu);
 navbarShoppingCart.addEventListener('click', toggleCartShopping);
@@ -44,7 +46,10 @@ function toggleCartShopping(){
 
     aside.classList.toggle('inactive');
 }
-/* ================================ */
+
+/* ================================ 
+    LISTA DE PRODUCTOS
+ ================================ */
 
 const listaProductos = [];
 
@@ -57,24 +62,44 @@ listaProductos.push({
 listaProductos.push({
     name: 'Monitor',
     price: 220,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    image: 'https://ohmygeek.net/wp-content/uploads/2021/06/Huawei-Display-23.8.jpg'
 });
 
 listaProductos.push({
     name: 'Computadora',
     price: 520,
+    image: 'https://images.pexels.com/photos/11833898/pexels-photo-11833898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+
+listaProductos.push({
+    name: 'bicicleta',
+    price: 120,
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+listaProductos.push({
+    name: 'Monitor',
+    price: 220,
+    image: 'https://ohmygeek.net/wp-content/uploads/2021/06/Huawei-Display-23.8.jpg'
+});
+
+listaProductos.push({
+    name: 'Computadora',
+    price: 520,
+    image: 'https://images.pexels.com/photos/11833898/pexels-photo-11833898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
 });
 
 for(producto of listaProductos){
     const productCard = document.createElement('div');
     productCard.classList.add('product-card')
 
-    const productImage = document.createElement('img');
-    productImage.setAtribbute('src', producto.image);
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', producto.image);
 
     const productInfo = document.createElement('div');
-    productInfo.classList.add('product-card')
+    productInfo.classList.add('product-info')
+
+    /* ======================================= */
 
     const productInfoDiv = document.createElement('div');
 
@@ -82,8 +107,25 @@ for(producto of listaProductos){
     productPrice.innerText= '$' + producto.price;
 
     const productName = document.createElement('div');
-    productName.innerText= '$' + producto.Name;
+    productName.innerText= '$' + producto.name;
 
-    const productInfoFigure  = document.createElement('div');
+    productInfoDiv.append(productPrice, productName);
+
+    /* ======================================= */
+    const productInfoFigure  = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg')
+
+    productInfoFigure.appendChild(productImgCart);
+
+    /* ======================================= */
+
+    productInfo.appendChild(productInfoDiv); 
+    productInfo.appendChild(productInfoFigure);
+
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+
+    cardsContainer.appendChild(productCard)
 
 }
